3245e3e931b61970ae16135805f90476dc6f1491
NiwarthanaSgm <nsgunaherath@gmail.com>
2022-01-26 14:59:07 +0530

    Added first component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-
+import part1 from './components/part1'
 function App() {
   return (
     <div className="App">
      <h1>Hello World!!!!!!!</h1>
      <p>This is my first time in working with ReactJS.</p>
+     <part1/>
     </div>
   );
 }
---
+++ src/components/part1.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function part1(){
+    return(
+        <div>
+            <h1>This is my first Component.</h1>
+        </div>
+    )
+}
